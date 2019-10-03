5582c5960190ee84fab2c78331061dcfc9865c93
pionkipl <jarek491@gmail.com>
2019-10-03 11:39:42 +0200

    Added typical directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'directives';
+  choice = '3';
+  color = 'blue';
+  fontSize = 12;
+
+  isBordered: boolean;
+  classesObj: object;
+  classList: string[];
+
+  people = [
+    {
+      name: 'Hola',
+      age: 25,
+      city: 'New York'
+    },
+    {
+      name: 'Mad',
+      age: 32,
+      city: 'Malb'
+    },
+    {
+      name: 'Roma',
+      age: 42,
+      city: 'Rome'
+    }
+  ]
+
+  ngOnInit() {
+    this.isBordered = false;
+    this.classList = ['blue', 'round'];
+    this.toggleBorder();
+  }
+
+  toggleBorder() {
+    this.isBordered = !this.isBordered;
+    this.classesObj = {
+      boardered: this.isBordered
+    };
+  }
+
+  apply(color: string, fontSize: number): void {
+    this.color = color;
+    this.fontSize = fontSize;
+  }
 }
